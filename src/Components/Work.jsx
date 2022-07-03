5f0bd4e51893cad9233cc0d0a71213e50f6a4152
Kenneth <obikennethe913@gmail.com>
2022-07-03 15:01:55 +0200

    job-listing updated

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -38,7 +38,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="https://job-listings-kennyoungc.vercel.app/">
+                <a href="https://job-listing-kenneth.netlify.app">
                   <button className="hover:bg-[#fe764c] text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
